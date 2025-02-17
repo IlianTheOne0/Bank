658322c4b948b885a86a27441c9844715b6c54b4
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-02-17 18:04:50 +0200

    Added queries to the database

---
+++ Data/Providers/clientProvider.h
@@ -0,0 +1,7 @@
+#ifndef CLIENT_PROVIDER_H
+#define CLIENT_PROVIDER_H
+
+#include "../../lib.h"
+#include "../../Core/core.h"
+
+#endif
--- Data/Source/Remote/queries.h
+++
@@ -1,6 +0,0 @@
-#ifndef DD_QUERIES_H
-#define DD_QUERIES_H
-
-
-
-#endif
