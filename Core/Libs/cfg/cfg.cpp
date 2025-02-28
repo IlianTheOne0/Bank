12a825b293227177934d155532132d344ba02e99
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-02-28 11:55:32 +0200

    Fixed

--- Core/Libs/cfg/cfg.cpp
+++ Core/Libs/cfg/cfg.cpp
@@ -10,4 +10,5 @@ namespace ConnectionConfig {
 #include "../../Logger/logger.h"
 bool Logger::_isEnabled = true;
 
+#include "../../../Presentation/Tests/tests.h"
 const unsigned short Test::testCounter = 4;
